9201aa73331a5492e2abc7b79734a63e8a4a2b3c
MH <michal.huras94@gmail.com>
2022-07-11 16:08:29 +0200

    Convert txt quiz file to SQLite database records

---
+++ src/database/convert_txt_to_sqlite.py
@@ -0,0 +1,27 @@
+"""Get data from quiz text file and put it into SQLite database file. Database version used in this script: 0.0.1. """
+from argparse import ArgumentParser, Namespace
+
+from src.database.database_manager import add_quiz
+from src.run import get_quiz_model
+
+DATABASE_PATH = 'data/quiz.db'
+
+
+def create_arg_parser() -> ArgumentParser:
+    """Create argument parser. """
+    pars = ArgumentParser(description=__doc__)
+    pars.add_argument('input_path', type=str, help='path to input text file to be processed')
+    pars.add_argument('database_path', type=str, help='path to SQLite database file to be processed')
+    return pars
+
+
+def run(arguments: Namespace):
+    """Main function of this script. """
+    model = get_quiz_model(arguments.input_path)
+    add_quiz(arguments.database_path, model)
+
+
+if __name__ == '__main__':
+    parser = create_arg_parser()
+    run(parser.parse_args())
+
