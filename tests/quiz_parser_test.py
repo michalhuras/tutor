5c9212e45ba220b30d8792f1b7cceb11e3c16a6c
MH <michal.huras94@gmail.com>
2022-07-11 16:09:51 +0200

    Quiz text file parser unit tests

--- tests/quiz_parser_test.py
+++ tests/quiz_parser_test.py
@@ -1,16 +1,19 @@
 from unittest import TestCase
 
-from src.question_model import QuizModel, QuestionModel
-from src.quiz_parser import parse_quiz_text
+from src.question_model import QuizModel, QuestionModel, AnswerModel
+from src.database.quiz_parser import parse_quiz_text
 
 
 class QuizParserTests(TestCase):
-    """ TODO """
-    def test_simple(self):
-        """ TODO """
+    """Unit tests for quiz text parsing function. """
+
+    def test_quiz_with_one_question(self):
+        """Test parsing quiz text with one question. """
         example_text ='''## Test quiz
 
 #### Q1. What is 2 + 2?
+
+![Q1](images/question1.png)
 
 - [ ] 1
 - [ ] 2
@@ -19,7 +22,68 @@ class QuizParserTests(TestCase):
 
 [reference](https://www.google.com/)
 '''
-        result_data = QuizModel(name='Test quiz', questions=[QuestionModel(text='Q1. What is 2 + 2?', answers=[
-            '1', '2', '3', '4'], correct_answer=3, comment='[reference](https://www.google.com/)')])
+        expected_data = QuizModel(
+            name='Test quiz',
+            questions=[
+                QuestionModel(
+                    text='Q1. What is 2 + 2?',
+                    answers=[
+                        AnswerModel('1', False),
+                        AnswerModel('2', False),
+                        AnswerModel('3', False),
+                        AnswerModel('4', True)],
+                    comment='[reference](https://www.google.com/)',
+                    image_path='images/question1.png')])
 
-        self.assertEqual(result_data, parse_quiz_text(example_text))
+        self.assertEqual(expected_data, parse_quiz_text(example_text))
+
+    def test_quiz_with_multiple_question(self):
+        """Test parsing quiz text with one question. """
+        example_text = '''## Test quiz
+
+#### Q1. What is 2 + 2?
+
+![Q1](images/question1.png)
+
+- [ ] 1
+- [ ] 2
+- [ ] 3
+- [x] 4
+
+[reference](https://www.google.com/)
+
+#### Q2. What color is a sky?
+
+- [ ] green
+- [x] blue
+- [ ] pink
+- [ ] dark
+
+**example**
+
+```
+Yes, sky is blue.
+```'''
+        expected_data = QuizModel(
+            name='Test quiz',
+            questions=[
+                QuestionModel(
+                    text='Q1. What is 2 + 2?',
+                    answers=[
+                        AnswerModel('1', False),
+                        AnswerModel('2', False),
+                        AnswerModel('3', False),
+                        AnswerModel('4', True)],
+                    comment='[reference](https://www.google.com/)',
+                    image_path='images/question1.png'),
+                QuestionModel(
+                    text='Q2. What color is a sky?',
+                    answers=[
+                        AnswerModel('green', False),
+                        AnswerModel('blue', True),
+                        AnswerModel('pink', False),
+                        AnswerModel('dark', False)],
+                    comment='**example**\n```\nYes, sky is blue.\n```',)
+            ])
+
+        self.assertEqual(expected_data, parse_quiz_text(example_text))
